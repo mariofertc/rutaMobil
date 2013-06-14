1abf73ef36efb956ba7a2edc2fc40ad616559dc0
Raul Jimenez <poetzle@yahoo.es>
2013-06-13 21:54:31 -0500

    Fotos y jquery
    ademas untrack de database.php

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'yoyo';
-$db['default']['database'] = 'rutamobil';
+$db['default']['password'] = '';
+$db['default']['database'] = 'rm';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
