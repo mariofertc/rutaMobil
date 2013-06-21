33b069a60fabf646245c1345c6f82c1cfd40b959
Raul Jimenez <poetzle@yahoo.es>
2013-06-21 00:17:02 -0500

    se cambio el contenido del footer page por  el correjido
    se cambio el contenido del footer page por  el correjido

--- application/views/mobile/partial/footer_page.php
+++ application/views/mobile/partial/footer_page.php
@@ -2,11 +2,13 @@
 
 		<div data-role="navbar" data-theme="e" class="pie">
             <ul>
-                <li><a href="#home" class="ui-btn-active" data-icon="home">Home</a></li>
-                <li><a href="#oferta" data-icon="grid">Oferta Turistica</a></li>
-                <li><a href="#aboutus" data-icon="info">About Us</a></li>
-                <li><a href="#geo" data-icon="star" onclick="sessionStorage.categoria_id=<?php echo isset($oferta->id)?$oferta->id:0;?>;sessionStorage.lugar_id=<?php echo isset($lugar->id)?$lugar->id:0;?>">Ubicacion</a></li>
+                <li title="inicio"><a href="#home" class="ui-btn-active"data-transition="slideup" data-icon="home">Inicio</a></li>
+                <li title="Sitios"><a href="#oferta" data-transition="slideup" data-icon="grid">Sitios</a></li>
+                <li title="Contáctanos" ><a href="#aboutus" data-transition="slideup" data-icon="info">Contáctanos</a></li>
+                <li title="ubicación"><a href="#geo" data-transition="slideup" data-icon="search">Ubicación</a></li>
             </ul>
         </div>
+
+
         <p class="copyright">&copy; 2012 Rutas moviles.com &nbsp;&nbsp;</p>
 </div>
