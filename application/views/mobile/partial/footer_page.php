a89516f60cd55c7c2aa49a487ef6928fae106bc7
Raul Jimenez <poetzle@yahoo.es>
2013-08-14 21:45:56 -0500

    personalización icono ubicación
    cambio icono ubicación por un personalizado

--- application/views/mobile/partial/footer_page.php
+++ application/views/mobile/partial/footer_page.php
@@ -5,10 +5,12 @@
                 <li title="inicio"><a href="#home" class="ui-btn-active"data-transition="slideup" data-icon="home">Inicio</a></li>
                 <li title="Sitios"><a href="#oferta" data-transition="slideup" data-icon="grid">Sitios</a></li>
                 <li title="Contáctanos" ><a href="#aboutus" data-transition="slideup" data-icon="info">Contáctanos</a></li>
-                <li title="ubicación"><a href="#geo" data-transition="slideup" data-icon="search"  onclick="sessionStorage.categoria_id=<?php echo isset($oferta->id)?$oferta->id:(isset($lugar->categoria_id)?$lugar->categoria_id:0);?>;sessionStorage.lugar_id=<?php echo isset($lugar->id)?$lugar->id:0;?>">Ubicación</a></li>
+                <li title="ubicación"><a href="#geo" data-transition="slideup" data-icon="phone-touch"  onclick="sessionStorage.categoria_id=<?php echo isset($oferta->id)?$oferta->id:(isset($lugar->categoria_id)?$lugar->categoria_id:0);?>;sessionStorage.lugar_id=<?php echo isset($lugar->id)?$lugar->id:0;?>">Ubicación</a></li>
             </ul>
         </div>
 
 
         <p class="copyright">&copy; 2012 Rutas moviles.com &nbsp;&nbsp;</p>
 </div>
+
+<!-- <a href="index.html" data-role="button" data-icon="smile" data-theme="b">data-icon="smile"</a> -->
